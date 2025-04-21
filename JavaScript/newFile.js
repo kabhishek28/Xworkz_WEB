b11016dd131cdbe050695f29011a6d6819adec2a
kabhishek28 <kabhishek.eng@gmail.com>
2025-04-21 22:26:15 +0530

    java script

---
+++ JavaScript/newFile.js
@@ -0,0 +1,29 @@
+    var age = 10
+    // var age = 48
+    // var age = 'abhi'
+    // age='abhishek'
+
+    let names = 'abhi28'
+    // let names = 'abhi10'
+    // names='abhi2003'
+
+    const institite='x-workz'
+    // institite = 'x-work'
+    // const institite= 37
+
+
+    document.writeln(age)
+
+    let emp1  = {id:12,employeeName:'aa',salary:1000}
+
+
+    let emp2 = new Object();
+    emp2.id=13;
+    emp2.employeeName = 'abhi';
+    emp2.salary=50000;
+
+    function emp3(id,employeeName,salary){
+        this.id = id;
+        this.employeeName = employeeName;
+        this.salary = salary;
+    }
