3fad822105e3e6957ea845f0d7e738b1ef9f4fc0
kabhishek28 <kabhishek.eng@gmail.com>
2025-04-24 23:55:40 +0530

    java scrpit

---
+++ onClick/script.js
@@ -0,0 +1,14 @@
+function SubmitForm(){
+    alert('dfghjkl');
+}
+
+function SubmitForm() {
+    let value = true;
+    let value1 = true;
+    let a = document.forName.nameId.value;
+    let b =  document.forName.numId.value;
+    alert(a);
+    alert(b);
+    return value;
+    
+}
